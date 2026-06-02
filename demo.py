8513047dc0ccc0dc7e0782c4669b603abb5052a3
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2026-06-02 19:32:30 +0700

    done

--- demo.py
+++ demo.py
@@ -4,3 +4,14 @@ database = 'database.sqlite'
 
 conn = sqlite3.connect(database)
 print('Opened data successfully')
+
+import pandas as pd
+tables = pd.read_sql("""SELECT *
+                     FROM sqlite_master
+                     WHERE type='table';""", conn)
+print(tables)
+
+matches = pd.read_sql("""SELECT *
+                      FROM MATCH;""", conn)
+
+print(matches.info())
